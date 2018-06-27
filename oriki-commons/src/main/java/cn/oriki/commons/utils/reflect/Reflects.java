a6914fad36f53774054414c21662d78ee4c8d807
SH-AD\oriki.wang <oriki.wang@chinascope.com>
2018-06-27 17:34:31 +0800

    create class Reflects

---
+++ oriki-commons/src/main/java/cn/oriki/commons/utils/reflect/Reflects.java
@@ -0,0 +1,197 @@
+package cn.oriki.commons.utils.reflect;
+
+import cn.oriki.commons.constants.ClassConstants;
+import cn.oriki.commons.utils.reflect.entity.FieldTypeNameValue;
+import cn.oriki.commons.utils.string.Strings;
+import com.google.common.collect.Lists;
+
+import java.io.Serializable;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * Reflects
+ *
+ * @author oriki.wang
+ */
+public class Reflects {
+
+    private static final String CLASS_PATH_SEPARATOR = "\\.";
+
+    /**
+     * 获取字节码对象
+     *
+     * @param classPath 全路径
+     * @return 字节码对象
+     * @throws ClassNotFoundException 全路径不能反射情况下抛出
+     */
+    public static Class getClass(String classPath) throws ClassNotFoundException {
+        if (Strings.isNotBlank(classPath)) {
+            return Class.forName(classPath);
+        }
+        return null;
+    }
+
+    /**
+     * 截取字节码文件的类名（如 java.lang.Object ， 截取获得 Object）
+     *
+     * @param clazz 字节码文件
+     * @param <T>   泛型
+     * @return Class 类名
+     */
+    public static <T> String getClassName(Class<T> clazz) {
+        String[] split = clazz.getName().split(CLASS_PATH_SEPARATOR);
+        return split[split.length - 1];
+    }
+
+    /**
+     * 获取字节码文件的所有 public method（包含父类）
+     *
+     * @param clazz 字节码文件
+     * @param <T>   泛型
+     * @return Class 对象中所有权限为 public 方法的对象
+     */
+    public static <T> List<Method> getPublicMethods(Class<T> clazz) {
+        return Lists.newArrayList(clazz.getMethods());
+    }
+
+    /**
+     * 获取字节码文件的所有 field (包含父类)
+     *
+     * @param clazz 字节码文件
+     * @param <T>   泛型
+     * @return 对象中所有成员变量
+     */
+    public static <T> List<Field> getFields(Class<T> clazz) {
+        return getFields(Lists.newArrayList(), clazz); // 递归获取父类 Field
+    }
+
+    /**
+     * 获取特定名称的 Field（包含父类）
+     *
+     * @param clazz     Class 对象
+     * @param fieldName 属性名称
+     * @param <T>       Class 泛型
+     * @return Field 对象，如果不存在，返回 null
+     */
+    public static <T> Field getField(Class<T> clazz, String fieldName) {
+        if (Strings.isBlank(fieldName)) {
+            return null;
+        }
+
+        for (Field field : getFields(clazz)) {
+            if (fieldName.equals(field.getName()))
+                return field;
+        }
+
+        return null;
+    }
+
+    /**
+     * 获取对象的属性 类型、名称和值（一定存在并且保证值为 Serializable 的子类） 集合
+     *
+     * @param entity 实体
+     * @param <S>    泛型
+     * @return 存在 value 的 FieldTypeNameValue
+     */
+    public static <S> List<FieldTypeNameValue> getFieldTypeNameValuesWithValuesIsNotNull(S entity) {
+        List<FieldTypeNameValue> fieldTypeNameValues = getFieldTypeNameValues(entity);
+
+        return fieldTypeNameValues.stream().filter(fieldTypeNameValue -> Objects.nonNull(fieldTypeNameValue.getValue()) && (fieldTypeNameValue.getValue() instanceof Serializable)).collect(Collectors.toList());
+    }
+
+    /**
+     * 获取对象的属性的 类型、名称和值 集合
+     *
+     * @param entity Object 对象
+     * @param <S>    泛型
+     * @return 对象中的成员变量类型、名称和值
+     */
+    public static <S> List<FieldTypeNameValue> getFieldTypeNameValues(S entity) {
+        List<FieldTypeNameValue> list = Lists.newArrayList();
+
+        List<Field> fields = getFields(entity.getClass());
+        fields.forEach((field -> {
+            try {
+                list.add(getFieldTypeNameValue(field, entity));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }));
+        return list;
+    }
+
+    /**
+     * 获取 Field 上的注解列表，如果不存在，返回空集合
+     *
+     * @param field Field 对象
+     * @return Field 对象上的注解集合
+     */
+    public static List<Annotation> getAnnotations(Field field) {
+        Annotation[] annotations = field.getDeclaredAnnotations();
+        return Lists.newArrayList(annotations);
+    }
+
+    /**
+     * 获取 Field 上的特定类型注解集合，如果不存在，返回 null
+     *
+     * @param field           Field 对象
+     * @param annotationClass 特定注解类型
+     * @return Field 对象上的注解
+     */
+    public static Annotation getAnnotation(Field field, Class annotationClass) {
+        Annotation annotation = field.getDeclaredAnnotation(annotationClass);
+        if (Objects.nonNull(annotation)) {
+            return annotation;
+        }
+        return null;
+    }
+
+    /**
+     * 获取类上的所有注解的集合，如果不存在，返回空集合
+     *
+     * @param clazz Class 文件
+     * @return 注解集合
+     */
+    public static List<Annotation> getAnnotations(Class clazz) {
+        Annotation[] annotations = clazz.getDeclaredAnnotations();
+        return Lists.newArrayList(annotations);
+    }
+
+    /**
+     * 获取类上的特定注解，如果不存在，返回null
+     *
+     * @param clazz      类的字节码文件
+     * @param annotation 特定注解的字节码文件
+     * @return 特定注解对象
+     */
+    public static Annotation getAnnotation(Class clazz, Class annotation) {
+        return clazz.getDeclaredAnnotation(annotation);
+    }
+
+    // 递归，获取 Class 本身及其父类中成员变量
+    private static <T> List<Field> getFields(List<Field> list, Class<T> clazz) {
+        Field[] fields = clazz.getDeclaredFields();
+        list.addAll(Arrays.asList(fields));
+        Class<? super T> superclass = clazz.getSuperclass();
+        if (!Objects.isNull(superclass) && !ClassConstants.JAVA_LANG_OBJECT.equals(superclass.getName())) {
+            getFields(list, superclass);
+        }
+        return list;
+    }
+
+    // 获取对象中对应成员变量的FieldTypeNameValue对象
+    private static <S> FieldTypeNameValue getFieldTypeNameValue(Field field, S entity) throws IllegalAccessException {
+        field.setAccessible(true); // 设置 Field 对象权限
+        Class<?> type = field.getType();
+        String name = field.getName();
+        Object o = field.get(entity);
+        return new FieldTypeNameValue(type, name, o);
+    }
+
+}
---
+++ oriki-commons/src/main/java/cn/oriki/commons/utils/reflect/entity/FieldTypeNameValue.java
@@ -0,0 +1,56 @@
+package cn.oriki.commons.utils.reflect.entity;
+
+import java.util.Objects;
+
+/**
+ * 成员变量的类型、名称、实体类的值
+ *
+ * @author oriki.wang
+ */
+public class FieldTypeNameValue {
+
+    private Class<?> type; // 成员变量类型
+    private String name; // 成员变量名称
+    private Object value; // 成员变量的值
+
+    public Class<?> getType() {
+        return type;
+    }
+
+    public void setType(Class<?> type) {
+        if (Objects.isNull(type))
+            throw new IllegalArgumentException("fieldType must be not null");
+
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (Objects.isNull(name))
+            throw new IllegalArgumentException("fieldName must be not null");
+
+        this.name = name;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public FieldTypeNameValue(Class<?> type, String name, Object value) {
+        // 保证 type 和 name 不能为 null
+        if (Objects.isNull(type) || Objects.isNull(name))
+            throw new IllegalArgumentException(getClass().getName() + " class instance fail , because fieldType and fieldName must be not null");
+
+        this.type = type;
+        this.name = name;
+        this.value = value;
+    }
+
+}
