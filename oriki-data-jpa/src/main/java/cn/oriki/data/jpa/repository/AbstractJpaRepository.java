2186465832ecd7d79e1ab8ad04f96d48e93edc16
SH-AD\oriki.wang <oriki.wang@chinascope.com>
2018-06-28 10:01:04 +0800

    add common method : getColumnName getTableName getPrimaryKeyColumnName

--- oriki-data-jpa/src/main/java/cn/oriki/data/jpa/repository/AbstractJpaRepository.java
+++ oriki-data-jpa/src/main/java/cn/oriki/data/jpa/repository/AbstractJpaRepository.java
@@ -1,10 +1,17 @@
 package cn.oriki.data.jpa.repository;
 
+import cn.oriki.commons.utils.string.StringConverts;
+import cn.oriki.commons.utils.string.Strings;
+import cn.oriki.data.annotation.Column;
+import cn.oriki.data.annotation.Table;
 import cn.oriki.data.repository.AbstractRepository;
+import cn.oriki.data.utils.reflect.ReflectDatas;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.Objects;
 
 public class AbstractJpaRepository<T, ID extends Serializable> extends AbstractRepository<T, ID> {
 
@@ -21,6 +28,70 @@ public class AbstractJpaRepository<T, ID extends Serializable> extends AbstractR
 
     public AbstractJpaRepository(Class T, Class ID) {
         super(T, ID);
+    }
+
+    /**
+     * 获取表名
+     * <p>
+     * 如果实体类没有设置 @Table 注解，按照标准类名转换为标准表名（ CustomerInformation -> t_customer_infomation）
+     * <p>
+     * 如果有 @Table 注解，直接使用注解的 name 属性作为表名
+     *
+     * @return 表名称
+     */
+    protected String getTableName() {
+        String tableName;
+        Table tableAnnotation = (Table) ReflectDatas.getAnnotation(entityClass, Table.class);
+        if (Objects.nonNull(tableAnnotation)) {
+            tableName = tableAnnotation.name();
+        } else {
+            String javaClassName = ReflectDatas.getClassName(entityClass); // 截取类名
+            tableName = StringConverts.toSQLTableName(javaClassName);
+        }
+
+        if (Strings.isBlank(tableName)) {
+            throw new IllegalArgumentException(" we can't get tableName , maybe @Table Annotation's name set null or \"\" ;");
+        }
+
+        return tableName;
+    }
+
+    /**
+     * 获取列名
+     * 默认获取 @Column 注解的名称
+     * 如果没有，自动转换标准列名（ userName -> user_name ）
+     *
+     * @return 数据库列名称
+     */
+    protected String getColumnName(Field field) {
+        String columnName;
+        Column annotation = (Column) ReflectDatas.getAnnotation(field, Column.class);
+        if (Objects.nonNull(annotation)) {
+            columnName = annotation.name();
+        } else {
+            columnName = StringConverts.toSQLColumnName(field.getName());
+        }
+
+        if (Strings.isBlank(columnName)) {
+            throw new IllegalArgumentException(" we can't get columnName , maybe @Column Annotation's name set null or \"\" ;");
+        }
+
+        return columnName;
+    }
+
+    /**
+     * 获取被 @PrimaryKey 标识的数据库列名称
+     * <p>
+     * 首先获取被 @PrimaryKey 标识的列，获取 Field
+     * <p>
+     * 获取其上 @Column 注解，获取 name 值；
+     * 如果不存在，转换标准列名
+     *
+     * @return 主键对应数据库列名
+     */
+    protected String getPrimaryKeyColumnName() {
+        Field field = getPrimaryKeyField();
+        return this.getColumnName(field);
     }
 
 }
--- oriki-data/src/main/java/cn/oriki/data/repository/AbstractRepository.java
+++ oriki-data/src/main/java/cn/oriki/data/repository/AbstractRepository.java
@@ -1,6 +1,13 @@
 package cn.oriki.data.repository;
 
+import cn.oriki.data.annotation.PrimaryKey;
+import cn.oriki.data.utils.reflect.ReflectDatas;
+
 import java.io.Serializable;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Objects;
 
 public abstract class AbstractRepository<T, ID extends Serializable> implements PagingAndSortRepository<T, ID> {
 
@@ -10,6 +17,37 @@ public abstract class AbstractRepository<T, ID extends Serializable> implements 
     public AbstractRepository(Class<T> T, Class<ID> ID) {
         this.entityClass = T;
         this.idClass = ID;
+    }
+
+    /**
+     * 获取被 @PrimaryKey 标识的属性，默认情况下一个实体类中只有一个
+     *
+     * @return 被 @PrimaryKey 标识的属性
+     */
+    protected Field getPrimaryKeyField() {
+        List<Field> fields = ReflectDatas.getFields(entityClass); // 获取实体及其父类的 Fields
+
+        Field _field = null;
+        int count = 0;
+
+        for (Field field : fields) {
+            List<Annotation> annotations = ReflectDatas.getAnnotations(field);
+            for (Annotation annotation : annotations) {
+                if (PrimaryKey.class.equals(annotation.annotationType())) {
+                    _field = field;
+                    count++;
+
+                    // check 条件：有且只有一个
+                    if (count > 1) {
+                        break;
+                    }
+                }
+            }
+        }
+        if (Objects.nonNull(_field) && count == 1) {
+            return _field;
+        }
+        throw new IllegalArgumentException("we need only one @PrimaryKey but find more than 1");
     }
 
 }
